68c3a347d745e333efab0708c963167f1d4f1e19
Linnea Lehmann Molander <chalottelinnea@hotmail.com>
2024-11-02 04:26:20 +0100

    Make username show up in canvas and scale changes

--- index.js
+++ index.js
@@ -2,5 +2,24 @@ let username;
 
 document.getElementById("submitBtn").onclick = function() {
     username = document.getElementById("userName").value;
-    console.log(username);
+
+    const canvas = document.getElementById("game");
+    const ctx = canvas.getContext("2d");
+
+    // Increase the canvas resolution for sharper text
+    const scale = window.devicePixelRatio || 1;
+    canvas.width = 600 * scale;
+    canvas.height = 600 * scale;
+    canvas.style.width = "600px";
+    canvas.style.height = "600px";
+
+    //scale the context 
+    ctx.scale(scale, scale);
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.font = "20px Arial";
+    ctx.fillstyle = "black";
+
+    ctx.fillText(username, 50, 20);
 }
